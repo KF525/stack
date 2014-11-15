16527de68b83caffbec3469188b1abab53a06765
Kate Fulton <katesuzannefulton@gmail.com>
2014-11-15 11:13:27 -0800

    Add push/pop functionality

--- spec/stack_spec.rb
+++ spec/stack_spec.rb
@@ -1,8 +1,29 @@
 require 'stack'
 
 describe Stack do
-  it "should be able to push" do
+  it "should be able to push and pop" do
     stack = Stack.new
     stack.push(3)
+    stack.pop.should eq 3
+  end
+
+  it "should return nil if you pop on an empty stack" do
+    stack = Stack.new
+    stack.pop.should eq nil
+  end
+
+  it "should return both values if you push twice and pop twice" do
+    stack = Stack.new
+    stack.push(3)
+    stack.push(4)
+    stack.pop.should eq 4
+    stack.pop.should eq 3
+  end
+
+  it "should return one value and nil if you push once and pop twice" do
+    stack = Stack.new
+    stack.push(3)
+    stack.pop.should eq 3
+    stack.pop.should eq nil
   end
 end
--- src/stack.rb
+++ src/stack.rb
@@ -1,4 +1,22 @@
 class Stack
+  def initialize
+    @stack = []
+    @i = 0
+  end
+
   def push(thing)
+    @i += 1
+    @stack[@i] = thing #[nil, 3, 4]
+  end
+
+  def pop
+    if @i != 0
+      popped = @stack[@i]
+      puts @stack.inspect
+      @i -= 1
+      popped
+    else
+      nil
+    end
   end
 end
